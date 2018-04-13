edd4cd42c0dfc9ee3f9435385915c9343e26da4d
yuratrumpe <yura.trumpe@gmail.com>
2018-04-13 12:07:12 +0300

    change to spring configuration

--- src/main/java/com/yuratrumpe/dao/factory/UserDaoFactoryImpl.java
+++ src/main/java/com/yuratrumpe/dao/factory/UserDaoFactoryImpl.java
@@ -4,6 +4,12 @@ import com.yuratrumpe.dao.UserDao;
 
 public class UserDaoFactoryImpl implements UserDaoFactory {
 
+    private String daoType;
+
+    public UserDaoFactoryImpl(String daoType) {
+        this.daoType = daoType;
+    }
+
     @Override
     public UserDao getUserDao() {
         return null;
