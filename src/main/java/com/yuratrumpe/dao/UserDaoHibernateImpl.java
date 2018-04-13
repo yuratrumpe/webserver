78b17c3c1ff5657d746b449eb929df41de8fc5ea
yuratrumpe <yura.trumpe@gmail.com>
2018-04-13 20:28:45 +0300

    change to spring configuration

--- src/main/java/com/yuratrumpe/dao/UserDaoHibernateImpl.java
+++ src/main/java/com/yuratrumpe/dao/UserDaoHibernateImpl.java
@@ -11,117 +11,64 @@ import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
 
-    private final SessionFactory sessionFactory;
+    private SessionFactory sessionFactory;
 
-    public UserDaoHibernateImpl(SessionFactory sessionFactory) {
+    public void setSessionFactory(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
 
     @Override
     public List<User> loadAllUsers() {
 
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-
-        try {
-            Query query = session.createQuery("FROM User");
-            return query.list();
-
-        } finally {
-            transaction.commit();
-            session.close();
-        }
-
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("FROM User").list();
     }
 
     @Override
     public User loadUserById(Long userId) {
 
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(User.class, userId);
 
-        try {
-//            Query query = session.createQuery("FROM User AS user WHERE user.id = :id");
-//            query.setParameter("id", userId);
-//            return (User) query.getSingleResult();
-            return session.get(User.class, userId);
-
-        } finally {
-            transaction.commit();
-            session.close();
-        }
     }
 
     @Override
     public User loadUserByName(String userName) {
 
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-
+        Session session = sessionFactory.getCurrentSession();
+        Query query = session.createQuery("FROM User AS user WHERE user.userName = :name");
+        query.setParameter("name", userName);
         try {
-            Query query = session.createQuery("FROM User AS user WHERE user.userName = :name");
-            query.setParameter("name", userName);
             User user = (User) query.getSingleResult();
             return user;
-
-
         } catch (NoResultException e) {
-
-          return null;
-
-        } finally {
-            transaction.commit();
-            session.close();
+            return null;
         }
+
     }
 
     @Override
     public Long storeUser(User user) {
 
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-
-        try {
-            session.save(user);
-            return user.getId();
-
-        } finally {
-            transaction.commit();
-            session.close();
-        }
+        Session session = sessionFactory.getCurrentSession();
+        session.save(user);
+        return user.getId();
 
     }
 
     @Override
     public void updateUser(User user) {
 
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-
-        try {
-            session.update(user);
-
-        } finally {
-            transaction.commit();
-            session.close();
-        }
+        Session session = sessionFactory.getCurrentSession();
+        session.update(user);
     }
 
     @Override
     public void deleteUser(Long userId) {
 
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-
-        try {
-            session.delete(new User(userId, null, null, null));
+        Session session = sessionFactory.getCurrentSession();
+        session.delete(new User(userId, null, null, null));
 //            session.delete(loadUserById(userId));
-
-        } finally {
-            transaction.commit();
-            session.close();
-        }
-
     }
 
 }
--- src/main/java/com/yuratrumpe/dao/factory/UserDaoFactoryImpl.java
+++ src/main/java/com/yuratrumpe/dao/factory/UserDaoFactoryImpl.java
@@ -5,13 +5,35 @@ import com.yuratrumpe.dao.UserDao;
 public class UserDaoFactoryImpl implements UserDaoFactory {
 
     private String daoType;
+    private UserDao userDaoJDBC;
+    private UserDao userDaoHibernate;
 
-    public UserDaoFactoryImpl(String daoType) {
+    public String getDaoType() {
+        return daoType;
+    }
+
+    public void setDaoType(String daoType) {
         this.daoType = daoType;
+    }
+
+    public UserDao getUserDaoJDBC() {
+        return userDaoJDBC;
+    }
+
+    public void setUserDaoJDBC(UserDao userDaoJDBC) {
+        this.userDaoJDBC = userDaoJDBC;
+    }
+
+    public UserDao getUserDaoHibernate() {
+        return userDaoHibernate;
+    }
+
+    public void setUserDaoHibernate(UserDao userDaoHibernate) {
+        this.userDaoHibernate = userDaoHibernate;
     }
 
     @Override
     public UserDao getUserDao() {
-        return null;
+        return daoType.equals("hibernate")?userDaoHibernate:userDaoJDBC;
     }
 }
